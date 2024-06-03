3e9781461204edec47a1c7db4b0e52f20769ab9d
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-06-04 01:32:01 +0530

    Update main2.cpp

--- Week 1/10. Functions in C++/main2.cpp
+++ Week 1/10. Functions in C++/main2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void Printmyname()
 {     //As, the return-type is VOID that's why we will not give any parameters to it. BUT IT ALSO TAKE PARAMETERS
     cout<<"Sahil"<<endl;
+    return; //But we can write this, as you can see I have not return anything which does not mean that I can't write return alone.
 }
 
 int main()
